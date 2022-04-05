74ddc7ca0d0ad9f3ebad0a0ae8af0467d2497bd1
Denish Shihora <dgshihora111@gmail.com>
2022-04-05 18:29:30 +0530

    1

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ function App() {
   return (
     <>
       <Topbar />
-      <Home />
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About1 />} />
         <Route path="/services" element={<Services1 />} />
@@ -22,7 +21,7 @@ function App() {
         <Route path="/contact" element={<Contacts1 />} />
         <Route path="/blog-grid" element={<BlogGrid1 />} />
         <Route path="/blog-details" element={<BlogDetails />} />
-      </Routes> */}
+      </Routes>
       <Footer />
     </>
   );
